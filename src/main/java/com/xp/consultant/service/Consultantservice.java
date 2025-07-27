a78d15ae2d688491933b2359f51b9cdb5fad9c0a
chongxue2580 <157788031+chongxue2580@users.noreply.github.com>
2025-07-27 14:54:06 +0800

    消息注解

--- src/main/java/com/xp/consultant/service/Consultantservice.java
+++ src/main/java/com/xp/consultant/service/Consultantservice.java
@@ -1,6 +1,9 @@
 package com.xp.consultant.service;
 
 
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
@@ -12,6 +15,12 @@ import reactor.core.publisher.Flux;
     )
 
 public interface Consultantservice {
-    public Flux<String> chat(String message);
-    public Flux<String> streamChat(String message);
+//用于聊天的方法
+//    @SystemMessage("你是智能管家")
+    @SystemMessage(fromResource="syetem.txt")
+//@UserMessage("你是智能小助手{{it}}")
+//    @UserMessage("你是智能小助手{{msg}}")
+
+    public Flux<String> chat(@V("msg")String message);
+
 }
